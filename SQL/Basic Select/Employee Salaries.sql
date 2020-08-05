6caf98b294310b35cb1b9153798ae471e3bd8c52
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-05 14:29:57 -0600

    Create Employee Salaries.sql

---
+++ SQL/Basic Select/Employee Salaries.sql
@@ -0,0 +1,8 @@
+problem: https://www.hackerrank.com/challenges/salary-of-employees/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+MySQL
+
+
+SELECT NAME
+FROM EMPLOYEE
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id;
