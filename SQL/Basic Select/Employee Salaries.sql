cedb387660433a46ae260cc4a2f946680076b6e0
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-05 22:00:21 -0600

    Update Employee Salaries.sql

--- SQL/Basic Select/Employee Salaries.sql
+++ SQL/Basic Select/Employee Salaries.sql
@@ -1,5 +1,5 @@
-problem: https://www.hackerrank.com/challenges/salary-of-employees/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
-MySQL
+# problem: https://www.hackerrank.com/challenges/salary-of-employees/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# MySQL
 
 
 SELECT NAME
