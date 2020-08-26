ff07edcc622f47220fcf0e7c8c01710e624a6838
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-26 10:50:04 -0600

    Update Top Competitors.sql

--- SQL/Basic Join/Top Competitors.sql
+++ SQL/Basic Join/Top Competitors.sql
@@ -1,4 +1,14 @@
 # problem: https://github.com/Parizat58/hackerrankPrac/new/master/SQL/Basic%20Join
 # MySQL
 
-basic join
+# The HAVING clause was added to SQL because the WHERE keyword could not be used with aggregate functions
+# The COUNT() function returns the number of rows that matches a specified criterion.
+
+select h.hacker_id, h.name
+from Submissions s inner join Challenges c on s.challenge_id = c.challenge_id 
+inner join Difficulty d on c.difficulty_level = d.difficulty_level
+inner join Hackers h on s.hacker_id = h.hacker_id
+where s.score = d.score and c.difficulty_level = d.difficulty_level
+group by h.hacker_id, h.name
+having count(s.hacker_id) > 1
+order by count(s.hacker_id) desc, s.hacker_id asc;
