3490686d1b69a70fd09b41f72264d446f9cd6297
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-26 10:53:45 -0600

    Update Top Competitors.sql

--- SQL/Basic Join/Top Competitors.sql
+++ SQL/Basic Join/Top Competitors.sql
@@ -3,6 +3,10 @@
 
 # The HAVING clause was added to SQL because the WHERE keyword could not be used with aggregate functions
 # The COUNT() function returns the number of rows that matches a specified criterion.
+# The GROUP BY statement groups rows that have the same values into summary rows, like "find the number of customers in each country".
+# The GROUP BY statement is often used with aggregate functions (COUNT, MAX, MIN, SUM, AVG) to group the result-set by one or more columns.
+
+
 
 select h.hacker_id, h.name
 from Submissions s inner join Challenges c on s.challenge_id = c.challenge_id 
