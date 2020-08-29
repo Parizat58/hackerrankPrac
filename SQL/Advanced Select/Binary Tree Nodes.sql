3e332185efeb543c8f39451fd5902f748d297660
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-29 15:44:47 -0600

    Create Binary Tree Nodes.sql

---
+++ SQL/Advanced Select/Binary Tree Nodes.sql
@@ -0,0 +1,12 @@
+
+# problem: https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+# MySQL
+
+
+SELECT CASE
+    WHEN P IS NULL THEN CONCAT(N, ' Root')
+    WHEN N IN (SELECT DISTINCT P FROM BST) THEN CONCAT(N, ' Inner')
+    ELSE CONCAT(N, ' Leaf')
+    END
+FROM BST
+ORDER BY N ASC
