19f0eacea9f4a77a70db9d3c473f204b98387a92
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-29 16:29:52 -0600

    Update Contest Leaderboard.sql

--- SQL/Basic Join/Contest Leaderboard.sql
+++ SQL/Basic Join/Contest Leaderboard.sql
@@ -1,4 +1,11 @@
 # problem: https://www.hackerrank.com/challenges/contest-leaderboard/problem
 # MySQL
 
+select H.hacker_id, H.name, sum(score) as total_score
+from Hackers as H inner join 
+(select hacker_id, max(score) as score from submissions group by challenge_id, hacker_id) max_score
 
+on H.hacker_id = max_score.hacker_id
+group by H.hacker_id, name
+having total_score >0
+order by total_score desc, H.hacker_id;
