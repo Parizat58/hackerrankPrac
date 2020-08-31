727fabf85bc52be57f114c98e198233984b7d0e9
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-31 17:11:31 -0600

    Create Top Earners.sql

---
+++ SQL/Aggregation/Top Earners.sql
@@ -0,0 +1,10 @@
+# problem: https://www.hackerrank.com/challenges/earnings-of-employees/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# MySQL
+
+
+
+SELECT salary * months AS earnings, COUNT(*)
+FROM Employee
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1;
