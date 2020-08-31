27a0a4bff9ef595d068b638c822aa87def25916b
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-31 16:44:39 -0600

    Create Revising Aggregations - The Sum Function.sql

---
+++ SQL/Aggregation/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,9 @@
+# problem: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?h_r=next-challenge&h_v=zen
+# MySQL
+
+
+
+
+select sum(population)
+from city 
+where district = 'California';
