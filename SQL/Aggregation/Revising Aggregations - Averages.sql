15fdc38f40d85a20c293031a3796147fff332a07
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-31 16:45:57 -0600

    Create Revising Aggregations - Averages.sql

---
+++ SQL/Aggregation/Revising Aggregations - Averages.sql
@@ -0,0 +1,8 @@
+# problem: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# MySQL 
+
+
+
+select avg(population)
+from city
+where district = 'California';
