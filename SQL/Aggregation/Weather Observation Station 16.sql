1e45e65b74171a55133d7886ba96809295633111
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-31 17:31:26 -0600

    Create Weather Observation Station 16.sql

---
+++ SQL/Aggregation/Weather Observation Station 16.sql
@@ -0,0 +1,9 @@
+# problem: https://www.hackerrank.com/challenges/weather-observation-station-16/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# MySQL
+
+
+select round(min(lat_n), 4)
+from station 
+where lat_n > 38.7780;
+
+
