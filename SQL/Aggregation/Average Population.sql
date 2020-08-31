168845f2e1eb9590683a28934626aa24fb072149
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-31 16:48:39 -0600

    Create Average Population.sql

---
+++ SQL/Aggregation/Average Population.sql
@@ -0,0 +1,8 @@
+# problem:https://www.hackerrank.com/challenges/average-population/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# MySQL
+
+select floor(avg(population))
+from city;
+
+
+
