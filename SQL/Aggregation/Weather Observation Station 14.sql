d5c778bb41abe373143a61a0772c4c733a6de0fa
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-08-31 17:25:54 -0600

    Create Weather Observation Station 14.sql

---
+++ SQL/Aggregation/Weather Observation Station 14.sql
@@ -0,0 +1,8 @@
+# problem: https://www.hackerrank.com/challenges/weather-observation-station-14/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# MySQL
+
+
+
+select truncate(max(lat_n), 4)
+from station
+where lat_n < 137.2345;
