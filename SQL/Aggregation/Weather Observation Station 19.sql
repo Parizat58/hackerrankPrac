3bc24939780f8cbf1a68e1f995d002693db822e9
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-09-02 16:05:35 -0600

    Create Weather Observation Station 19.sql

---
+++ SQL/Aggregation/Weather Observation Station 19.sql
@@ -0,0 +1,20 @@
+# problem: https://www.hackerrank.com/challenges/weather-observation-station-19/problem?h_r=next-challenge&h_v=zen
+# MySQL
+
+
+
+
+# SQRT() returns the square root of a given value in the argument. SQRT(25);  OUTPUT: 5
+
+
+SELECT
+    ROUND(SQRT(
+        POWER(MAX(LAT_N)  - MIN(LAT_N),  2)
+      + POWER(MAX(LONG_W) - MIN(LONG_W), 2)
+    ), 4)
+FROM 
+    STATION;
+
+
+
+
