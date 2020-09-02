dd842332265e44ba6becd767ecb9545b9ef8c5cb
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-09-02 16:12:17 -0600

    Create Weather Observation Station 20.sql

---
+++ SQL/Aggregation/Weather Observation Station 20.sql
@@ -0,0 +1,13 @@
+# problem: https://www.hackerrank.com/challenges/weather-observation-station-20/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+# Oracle
+SELECT ROUND(MEDIAN(Lat_N), 4)
+FROM Station;
+
+
+
+
+
+
+
