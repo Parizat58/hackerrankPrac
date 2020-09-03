dc607cf2e366301f1abab2e9028dbb76eb3e1b4f
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-09-03 10:45:50 -0600

    Update Draw The Triangle 1.sql

--- SQL/Alternative Queries/Draw The Triangle 1.sql
+++ SQL/Alternative Queries/Draw The Triangle 1.sql
@@ -2,3 +2,16 @@
 # MySQL
 
 
+# REPEAT() repeats a string for a specified number of times. Both the string and the number of times that string to be repeated are supplied as arguments.
+# REPEAT(str, count)
+# The following MySQL statement repeats the string ‘**-‘ 15 times. 
+# SELECT REPEAT('**-',15); 
+#  for any sql query you need a table. Since, there is no given table in the question, we have to use something else. Thankfully, sql comes with information_schema.tables built-in.
+# There are two ways to assign a value to a user-defined variable. The first way is to use the SET statement as follows: SET @variable_name := value;
+# You can use either := or = as the assignment operator in the SET statement. For example, the statement assigns number 100 to the variable @counter. SET @counter := 100;
+# 
+
+
+set @number := 21;
+select repeat('* ', @number := @number - 1)
+from information_schema.tables;
