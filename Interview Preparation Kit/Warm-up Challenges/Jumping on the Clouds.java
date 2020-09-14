5688641f8e9f0aca17a5970454dd5e628ecdcdab
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-09-14 11:22:36 -0600

    Update Jumping on the Clouds.java

--- Interview Preparation Kit/Warm-up Challenges/Jumping on the Clouds.java
+++ Interview Preparation Kit/Warm-up Challenges/Jumping on the Clouds.java
@@ -1,6 +1,19 @@
 # problem: https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 
 
+static int jumpingOnClouds(int[] c) {
+    int count = 0;
 
+    for(int i = 0; i < c.length-1; i++){
+        if(c[i] ==0){
+            count++;
+        }
+        if(i+2 < c.length && c[i+2] ==0){
+        i++;
+        }
+    }
+    return count;
+
+    }
 
 
