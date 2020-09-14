4eead4ba789ffe0995c9dd2c448aedb8ab1f681e
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-09-14 10:47:35 -0600

    Update Counting Valleys.java

--- Interview Preparation Kit/Warm-up Challenges/Counting Valleys.java
+++ Interview Preparation Kit/Warm-up Challenges/Counting Valleys.java
@@ -1,5 +1,24 @@
 # problem: https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup&h_r=next-challenge&h_v=zen
 
 
+  
+  public static int countingValleys(int steps, String path) {
+    // Write your code here
+    int valley = 0;
+    int level = 0;
+    boolean belowSea = false;
+    for(int i =0; i < steps; i++){
+        char slope = path.charAt(i);
+        if(slope == 'U') level++;
+        else level--;
 
-
+        if(!belowSea && level <0){
+            valley++;
+            belowSea = true;
+        }
+        if(level >= 0){
+            belowSea = false;
+        }
+    }System.out.println(valley);
+    return valley;
+    }
