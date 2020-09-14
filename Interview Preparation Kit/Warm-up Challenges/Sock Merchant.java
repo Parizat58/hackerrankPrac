144f0a044db3573d60cda4521830821ccade7078
Parizat58 <52273275+Parizat58@users.noreply.github.com>
2020-09-14 09:52:09 -0600

    Update Sock Merchant.java

--- Interview Preparation Kit/Warm-up Challenges/Sock Merchant.java
+++ Interview Preparation Kit/Warm-up Challenges/Sock Merchant.java
@@ -1,1 +1,19 @@
+#problem(https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+)
+
+ // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+      Set<Integer> colors = new HashSet<>();
+      int pairs = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!colors.contains(ar[i])) {
+            colors.add(ar[i]);
+        } else {
+            pairs++;
+            colors.remove(ar[i]);
+        }
+    }
+    return pairs;
+    } 
 
